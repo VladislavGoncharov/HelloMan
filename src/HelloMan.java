a791ad79344e51926dfb4a51424671dc8a3f54aa
VladislavGoncharov <veyvik87@gmail.com>
2021-10-14 20:03:54 +0300

    man is big

--- src/HelloMan.java
+++ src/HelloMan.java
@@ -1,8 +1,7 @@
 public class HelloMan {
     public static void main(String[] args) {
         System.out.println("HelloMan");
-        int hello = 1;
-        int man = 2;
-        System.out.println(hello + man);
+        String big = "Man is big";
+        System.out.println(big);
     }
 }
